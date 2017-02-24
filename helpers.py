f51b26d11a469170d2acfe5d31822a142aa0fc75
nguyenwinle <ucnguyenle@gmail.com>
2017-02-24 15:15:29 -0800

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -12,9 +12,6 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
 
-# my secret code 
-secret = 'winter'
-
 def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
